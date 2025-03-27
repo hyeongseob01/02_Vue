1feefeb05a4ac4c8dcf67d4399303f24a18b692b
임형섭 <imhyeongseob@imhyeongseob-ui-MacBookPro.local>
2025-03-27 15:18:10 +0900

    20250327 기본 과제

---
+++ Practice/20250327/calc-component-test/src/components/Calc5.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        x : <input type="text" v-model.number="x">
+        <br>
+        결과 : {{ result }}
+    </div>
+</template>
+<script>
+import { ref, watch } from 'vue';
+export default{
+    name:'Calc5',
+    setup(){
+        const x = ref(0);
+        const result = ref(0);
+        watch(x,(newVal) =>{
+            console.log('x가 변경됨:', newVal);
+            result.value = x.value
+        })
+        return{
+            x, result
+        };
+    }
+}
+</script>
